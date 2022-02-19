f26657cead5d579264677c409b0a56606115aa7f
Kat Camp <kcamp552369@gmail.com>
2022-02-19 14:41:23 -0500

    starting main menu and breakdown

--- src/com/careerdevs/Main.java
+++ src/com/careerdevs/Main.java
@@ -1,9 +1,28 @@
 package com.careerdevs;
 
-public class Main {
 
+
+public class Main {
     public static void main(String[] args) {
-// main menu for options
+       System.out.print("Welcome to Guess the WORD!!!");
+
+        mainMenu();
+        System.out.print("Thank you for Playing");
+            }
+        public static  mainMenu(){
+
+        }
+
+        public static wordGen(){
+
+        }
+        public static void finish(){
+
+        }
+        public static void play(){
+
+        }
+}
 // start game, end game
 
 // pick a word
@@ -22,5 +41,45 @@ public class Main {
 
 
 
-    }
-}
+
+//        public static void play(String[] args) {
+//            int numOfGuesses = 0;
+//
+//            boolean iterated = false;
+//            String temp = "";
+//            String currentCorrect = "";
+//
+//            String word = "careerdevs" ;
+//
+//            do {
+//
+//                String guess = CLI.readString("What is your guess?");
+//
+//                for(int i = 0; i < word.length(); i ++) {
+//                    if (guess.equals(Character.toString(word.charAt(i)))) {
+//                        if(!iterated)
+//                            temp += Character.toString(word.charAt(i));
+//                        else {
+//                            currentCorrect = Character.toString(temp.charAt(i)).replace("-", guess);
+//                            temp = temp.substring(0, i) + currentCorrect + temp.substring( i + 1, temp.length());
+//                        }
+//                    } else {
+//                        if(!iterated) {
+//                            temp += "-";
+//                        }
+//                    }
+//                }
+//                numOfGuesses++;
+//                iterated = true;
+//                System.out.println(temp);
+//                if(temp.equals(word)) {
+//                    System.out.println("You guessed correctly!");
+//                    break;
+//                }
+//            }while (numOfGuesses < 10);
+//
+//        }
+//    }
+//
+// //   }
+//
