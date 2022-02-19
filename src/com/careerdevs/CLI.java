53209d0d8c810fee9ea4d2c2e6cc6175f7fdddab
Kat Camp <kcamp552369@gmail.com>
2022-02-18 23:09:11 -0500

    Initial commit

---
+++ src/com/careerdevs/CLI.java
@@ -0,0 +1,68 @@
+package com.careerdevs;
+
+import java.util.InputMismatchException;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class CLI {
+    private static  Scanner scanner  = new Scanner(System.in);
+
+    public static String readString (String question ) {
+        while (true) {
+            System.out.print(question);
+            String inputString = scanner.nextLine();
+
+            if (!inputString.trim().equals("")) {
+                return inputString;
+            }
+
+            System.out.print(question);
+            return scanner.nextLine();
+        }
+    }
+
+    public static int readInt (String question, int min, int max){
+        try {
+            System.out.print(question);
+            System.out.print("Number( " + min + "- " + max + ")");
+            int temp = scanner.nextInt();
+            scanner.nextLine();
+
+            if (temp >= min && temp <= max){
+                return temp;
+            }
+            System.out.println("Number outside of the valid range, Please try again");
+            return readInt(question, min,max);
+
+        }catch(InputMismatchException e){
+            System.out.print("Must enter a valid integer, please try again");
+            scanner.nextLine();
+            return readInt(question,min,max);
+        }catch (ArrayIndexOutOfBoundsException e){
+            System.out.print("Must enter a valid selection, please try again");
+            scanner.nextLine();
+            return readInt(question,min,max);
+        }
+
+
+    }
+
+    public static boolean  yesOrNo (String question){
+        while(true){
+            System.out.print("\n" + question + "\n yes or no:");
+            String readInputString = scanner.nextLine();
+            char finalInput = readInputString.toLowerCase(Locale.ROOT).trim().charAt(0);
+
+            if( finalInput == 'y'){
+                return true;
+            }else if ( finalInput == 'n'){
+                return false;
+            }
+            System.out.print("Input must be Y or N");
+        }
+
+    }
+
+}
+
+
---
+++ src/com/careerdevs/Main.java
@@ -0,0 +1,25 @@
+package com.careerdevs;
+
+public class Main {
+
+    public static void main(String[] args) {
+// main menu for options
+// start game, end game
+
+// pick a word
+//make an array with entire alphabet
+// make an array with letters that have been choosen
+// take in guess
+// move letter to choosen array
+// run through to see if is in word
+//print results
+
+//option t ocontinue gae or exit
+
+// word is completed
+// game won
+
+
+
+    }
+}
